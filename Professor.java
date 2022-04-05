e05ad5b3d531f51139abaa6eb390fcd7671e55a7
Gaby <gaabyrocha574@gmail.com>
2022-04-05 18:37:48 +0000

    p1 interface

--- Professor.java
+++ Professor.java
@@ -1,26 +1,4 @@
 public class Professor extends Pessoa {
     
-    private String nome;
-    private String cpf;
-    private String disciplina;
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getDisciplina() {
-        return disciplina;
-    }
-    public void setDisciplina(String materia) {
-        this.disciplina = disciplina;
-    }
 
 }
