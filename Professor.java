7ec9992068620ac6069d58c19cffb682b2fe752e
Gaby <gaabyrocha574@gmail.com>
2022-04-05 17:53:27 +0000

    interface prova p1

---
+++ Professor.java
@@ -0,0 +1,26 @@
+public class Professor extends Pessoa {
+    
+    private String nome;
+    private String cpf;
+    private String disciplina;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getMateria() {
+        return disciplina;
+    }
+    public void setMateria(String materia) {
+        this.disciplina = materia;
+    }
+
+}
